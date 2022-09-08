11a918b38c8045734dbd9d947f23236c03a2b6b3
Tom Spencer <thomas.spencer@codurance.com>
2022-09-08 13:41:43 +0100

    add code

---
+++ src/test/java/sort/SortTest.java
@@ -0,0 +1,36 @@
+package sort;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class SortTest {
+
+  @Test
+  void sortTest() {
+    assertEquals(asList(), sort(asList()));
+    assertEquals(asList(1), sort(asList(1)));
+    assertEquals(asList(1, 2), sort(asList(1, 2)));
+    assertEquals(asList(1, 2), sort(asList(2, 1)));
+    assertEquals(asList(1, 2, 3), sort(asList(1, 2, 3)));
+    assertEquals(asList(1, 2, 3), sort(asList(2, 1, 3)));
+    assertEquals(asList(1, 2, 3), sort(asList(1, 3, 2)));
+  }
+
+  private List<Integer> sort(List<Integer> list) {
+    if (list.size() > 1) {
+      int firstIndex = 0;
+      int secondIndex = firstIndex + 1;
+      if (list.get(firstIndex) > list.get(secondIndex)) {
+        int first = list.get(firstIndex);
+        int second = list.get(secondIndex);
+        list.set(firstIndex, second);
+        list.set(secondIndex, first);
+      }
+    }
+    return list;
+  }
+}
