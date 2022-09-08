3e4b40db548dca0585982374bcbe5e1c47e0e0d7
Tom Spencer <thomas.spencer@codurance.com>
2022-09-08 13:52:28 +0100

    Add outer for loop

--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -18,17 +18,20 @@ public class SortTest {
     assertEquals(asList(1, 2, 3), sort(asList(1, 2, 3)));
     assertEquals(asList(1, 2, 3), sort(asList(2, 1, 3)));
     assertEquals(asList(1, 2, 3), sort(asList(1, 3, 2)));
+    assertEquals(asList(1, 2, 3), sort(asList(3, 2, 1)));
   }
 
   private List<Integer> sort(List<Integer> list) {
     if (list.size() > 1) {
-      for (int firstIndex = 0; firstIndex < list.size() - 1; firstIndex++) {
-        int secondIndex = firstIndex + 1;
-        if (list.get(firstIndex) > list.get(secondIndex)) {
-          int first = list.get(firstIndex);
-          int second = list.get(secondIndex);
-          list.set(firstIndex, second);
-          list.set(secondIndex, first);
+      for (int limit = list.size() - 1; limit > 0; limit--) {
+        for (int firstIndex = 0; firstIndex < limit; firstIndex++) {
+          int secondIndex = firstIndex + 1;
+          if (list.get(firstIndex) > list.get(secondIndex)) {
+            int first = list.get(firstIndex);
+            int second = list.get(secondIndex);
+            list.set(firstIndex, second);
+            list.set(secondIndex, first);
+          }
         }
       }
     }
